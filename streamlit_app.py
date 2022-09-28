6d2b0c1dc11e92d6fb14eb0c9b2e435dca3b6866
Pajaniappan <114441824+pajaniappan@users.noreply.github.com>
2022-09-28 15:57:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,6 @@ streamlit.header('Womens design')
 streamlit.text('👗 Dress')
 streamlit.text('👙 Bikini')
 streamlit.text('👜 Handbag')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
